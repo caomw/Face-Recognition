b075d14467fa110e6f7df94e3845d999b052a4f1
ericpairet <ericpairet@gmail.com>
2015-12-12 21:16:47 +0100

    Deleting temporal files when closing the GUI

--- code/face_recognition.m
+++ code/face_recognition.m
@@ -1,7 +1,7 @@
+% Face recognition project, 2015 - 2016
 %
-%
-%
-%
+% Eric Pairet Artau: ericpairet@gmail.com
+% Songyou Peng: psy920710@gmail.com
 %
 
 % Clear workspace
@@ -21,14 +21,14 @@ h_gui = gui;
 h_pca = pca;
 
 % Clean normalization folders
-rmdir( '../train_images/_norm/' , 's' );                                        % Not necessary if these folders are eliminated once finished the program
-rmdir( '../test_images/_norm/' , 's' );
 mkdir( '../train_images/_norm/' );
 mkdir( '../test_images/_norm/' );
 
-% Call GUI
+% Call GUI and save item handlers
 h_items = h_gui.createGui( h_files , h_normalization , h_pca );
 
-% Eliminate of the temporal files
-%rmdir( '../train_images/_norm/' , 's' );
-%rmdir( '../test_images/_norm/' , 's' );
+% Eliminate temporal files
+uiwait( h_items.hFig );
+rmdir( '../train_images/_norm/' , 's' );
+rmdir( '../test_images/_norm/' , 's' );
+disp('Temporal files deleted');
